5c029eaae3ecd233f34d1ddd85d612ef5511db78
AratzEguren <egurenaratz@gmail.com>
2024-11-29 12:56:39 +0100

    Mi primer commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name' : 'RovoBank',
+    'version' : '1.0',
+    'description' : 'The bank that heps you.',
+    'author' : 'Grupo4',
+    'data' : [
+
+    ],
+
+}
---
+++ models/loan.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+from odoo.release import description
+
+
+class Loan (models.Model) :
+    _name = "grupo4.loan"
+    _description = "guarda los prestamos de cada usuario"
+
+
+    # campos simples
+    interestRate = fields.Integer(string="El porcentage de interes del prestamo")
+    endDate = fields.Date(string="La fecha limite en la que el prestamo ha de ser pagado")
+    remainingBalance = fields.Float(string= "El monto que falta por pagar")
+    period = fields.Integer(string="La periodicidad con la que se paga el prestamo en dias")
+    description = fields.Char(string="Descripción personal del motivo del prestamo")
