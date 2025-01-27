83debfdce55d9aee9d4f9616367e13123c4c59c4
irodrigon <irodrigon2313@gmail.com>
2025-01-27 12:18:10 +0100

    modificadas herencia y menu.

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'version' : '1.0',
     'description' : 'The bank that helps you',
     'autor' : 'Grupo4',
-    'depends': ['base', 'auth_signup'],
+    'depends': ['base', 'auth_signup','mail'],
     'data' : [
         'security/rovobank_security.xml',
         'security/ir.model.access.csv',
--- models/User.py
+++ models/User.py
@@ -3,31 +3,14 @@ from odoo import fields,models
 class User(models.Model):
     _name = "grupo4.user"
     _description = "Rovobank's users"
-    _inherit ={'res.partner':'name',
-               'res.partner':'street',
-               'res.partner':'city',
-               'res.partner':'active',
-               'res.partner':'zip'}
 
-    def init(self):
-        # Check if the column already exists
-        self.env.cr.execute("""
-               SELECT column_name 
-               FROM information_schema.columns 
-               WHERE table_name='res_partner' and column_name='signup_token'
-           """)
-        # If the column does not exist, add it
-        if not self.env.cr.fetchone():
-            self.env.cr.execute("ALTER TABLE res_partner ADD COLUMN signup_token varchar")
+    username = fields.Char(string="username")
+    name = fields.Char(string="name")
+    surname = fields.Char(string="surname")
+    street = fields.Char(string="street")
+    city = fields.Char(string="city")
+    zip = fields.Char(string="zip")
 
-    username = fields.Char(String="Username",required=True)
 
-    channel_ids = fields.Many2many(
-        comodel_name='mail.channel',
-        relation='mail_channel_res_partner_grupo4_user_rel',
-        column1='res_partner_grupo4_user_id',
-        column2='channel_id',
-        string="Channels"
-    )
 
 
